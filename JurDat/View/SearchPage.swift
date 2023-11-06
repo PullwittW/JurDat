9e2a40e868518e0041a93ea055edd682404f89b4
Wangu Pullwitt <wangupullwitt.priv@gmail.com>
2023-11-06 20:35:57 +0100

    Completing tab and search bar

---
+++ JurDat/View/SearchPage.swift
@@ -0,0 +1,18 @@
+//
+//  SearchPage.swift
+//  JurDat
+//
+//  Created by Wangu Pullwitt on 06.11.23.
+//
+
+import SwiftUI
+
+struct SearchPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchPage()
+}
