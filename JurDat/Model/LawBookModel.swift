0eca166ee79b54ea068ce98e3c53655cf983b715
Wangu Pullwitt <wangupullwitt.priv@gmail.com>
2023-11-07 00:17:12 +0100

    Laws View

---
+++ JurDat/Model/LawBookModel.swift
@@ -0,0 +1,8 @@
+//
+//  LawBookModel.swift
+//  JurDat
+//
+//  Created by Wangu Pullwitt on 06.11.23.
+//
+
+import Foundation
---
+++ JurDat/ViewModel/LawBookViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  LawViewModel.swift
+//  JurDat
+//
+//  Created by Wangu Pullwitt on 06.11.23.
+//
+
+import Foundation
+
+class LawViewModel: ObservableObject {
+    
+}
