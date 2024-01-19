a8fbeaf2da57e03485bc579c2f360468379b7ddf
Wangu Pullwitt <wangupullwitt.priv@gmail.com>
2024-01-19 19:51:10 +0100

    19.01.24 updated sign up process and views

---
+++ JurDat/Core/Authentication/Subviews/SignUp/SignUpLastName.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpLastName.swift
+//  JurDat
+//
+//  Created by Wangu Pullwitt on 19.01.24.
+//
+
+import SwiftUI
+
+struct SignUpLastName: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpLastName()
+}
