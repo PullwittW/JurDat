e3ceb8a2d411c3cf725d9592e79fcd2c2b4668fb
Wangu Pullwitt <wangupullwitt.priv@gmail.com>
2024-01-27 11:07:21 +0100

    Added TabBar View (first init)

---
+++ JurDat/Core/TabBar/TabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarView.swift
+//  JurDat
+//
+//  Created by Wangu Pullwitt on 27.01.24.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBarView()
+}
